da5c9f7019226aa75b3cf7f92354966b7e64c863
buliktt <tynymbaevba@gmail.com>
2015-07-14 19:12:33 +0600

    Create models.py

---
+++ models.py
@@ -0,0 +1,34 @@
+from flask.ext.sqlalchemy import SQLAlchemy
+from werkzeug import generate_password_hash, check_password_hash
+
+db = SQLAlchemy()
+
+class User(db.Model):
+  __tablename__ = 'users'
+  uid = db.Column(db.Integer, primary_key = True)
+  firstname = db.Column(db.String(100))
+  lastname = db.Column(db.String(100))
+  email = db.Column(db.String(120), unique=True)
+  pwdhash = db.Column(db.String(54))
+  
+  def __init__(self, firstname, lastname, email, password):
+    self.firstname = firstname.title()
+    self.lastname = lastname.title()
+    self.email = email.lower()
+    self.set_password(password)
+    
+  def set_password(self, password):
+    self.pwdhash = generate_password_hash(password)
+  
+  def check_password(self, password):
+    return check_password_hash(self.pwdhash, password)
+
+class Incident(db.Model):
+	__tablename__ = 'incidents'
+	uid = db.Column(db.Integer, primary_key = True)
+	body = db.Column(db.String(140))
+	#timestamp = db.Column(db.Datetime)
+	user_uid = db.Column(db.Integer, db.ForeignKey('user.uid'))
+	
+	def __repr__(self):
+		return '<Incident %r>' % (self.incident)
